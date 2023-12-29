116f337b7c0bd47455f69141dea88f9c74b88b5c
yuki <marinaolouis18@gmail.com>
2023-12-29 13:24:56 +0000

    form added

--- createTable.php
+++ createTable.php
@@ -31,15 +31,106 @@ $selectedDatabase = isset($_GET['databases']) ? $_GET['databases'] : null;
 <body>
     <div class="flex h-screen bg-gray-50 dark:bg-gray-900" :class="{ 'overflow-hidden': isSideMenuOpen }">
         <!-- Desktop sidebar -->
-        <?php require_once 'components/sidebar.php';?>
+        <?php require_once 'components/sidebar.php'; ?>
         <div class="flex flex-col flex-1 w-full">
             <!-- Header -->
-            <?php require_once 'components/header.php';?>
+            <?php require_once 'components/header.php'; ?>
 
             <main class="h-full overflow-y-auto">
-                <div class="container grid px-6 mx-auto">
-<!-- Create table form for database -->
-              
+                <div class="container grid px-6 mt-10 mx-auto">
+
+                    <h1 class="text-xl font-bold">Create Table</h1>
+
+                    <form class="w-full mx-auto">
+                        <div class="relative z-0 w-full mb-5 group">
+                            <input type="text" name="floating_text" id="floating_text"
+                                class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                                placeholder=" " required />
+                            <label for="floating_text"
+                                class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
+                                Table Name</label>
+                        </div>
+
+                        <h1 class="text-xl font-bold">Collumns</h1>
+
+                        <div class="grid md:grid-cols-6 md:gap-6">
+                            <div class="relative z-0 w-full mb-5 group">
+                                <input type="text" name="floating_first_name" id="floating_first_name"
+                                    class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                                    placeholder=" " required />
+                                <label for="floating_first_name"
+                                    class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
+                                    name</label>
+                            </div>
+                            <div class="relative z-0 w-full mb-5 group">
+                                <input type="text" name="floating_data_type" id="floating_data_type"
+                                    class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                                    placeholder=" " required />
+                                <label for="floating_data_type"
+                                    class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
+                                    data Type</label>
+                            </div>
+                            <div class="relative z-0 w-full mb-5 group">
+                                <input type="text" name="floating_size" id="floating_size"
+                                    class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                                    placeholder=" " required />
+                                <label for="floating_size"
+                                    class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
+                                    Length</label>
+                            </div>
+                            <div class="relative z-0 w-full mb-5 group">
+                                <Select name="floating_Index" id="floating_Index"
+                                    class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer">
+                                    <option value=""></option>
+
+                                    <option value="PRIMARY">Primary</option>
+                                </Select>
+                                <label for="floating_Index"
+                                    class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
+                                    Index</label>
+                            </div>
+                            <div class="relative z-0 w-full mb-5 group">
+                                <div class="flex items-center mb-4">
+                                    <input id="default-checkbox" type="checkbox" value="" name="auto_increment"
+                                        class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                                    <label for="default-checkbox"
+                                        class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Auto
+                                        Increment</label>
+                                </div>
+                            </div>
+                            <!-- action div delete and add svg flowbite style -->
+                            <div class="flex items-center justify-center">
+                         
+
+                                <button type="submit" name="submit" id="submit" class="text-white bg-red-600 hover:bg-red-700 focus:ring-4 focus:outline
+                                focus:ring-red-300 font-medium rounded-lg p-3
+                                text-center dark:bg-red-500 dark:hover:bg-red-600 dark:
+                                focus:ring-red-800"><svg class="w-3 h-3 text-gray-800 dark:text-white"
+                                        aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                                        viewBox="0 0 14 14">
+                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                            stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                                    </svg></button>
+
+                            </div>
+
+
+                        </div>
+
+                        <button type="submit" name="add_column"
+                            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+                        <button type="button" class="text-white bg-lime-600 hover:bg-lime-700 focus:ring-4 focus:outline
+                                focus:ring-lime-300 font-medium rounded-lg p-3
+                                text-center dark:bg-lime-500 dark:hover:bg-lime-600 dark:
+                            focus:ring-blue-800"><svg class="w-4 h-4 text-gray-800 dark:text-white" aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                    stroke-width="2" d="M9 1v16M1 9h16" />
+                            </svg></button>
+
+                    </form>
+
+
                 </div>
             </main>
         </div>
