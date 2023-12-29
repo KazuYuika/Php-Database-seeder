5d682667723dd48403c47d3c10aa370fe711a264
yuki <marinaolouis18@gmail.com>
2023-12-29 12:07:13 +0000

    chanfed

--- home.php
+++ home.php
@@ -166,7 +166,7 @@ $selectedDatabase = isset($_GET['databases']) ? $_GET['databases'] : null;
                                     <label for="small-input"
                                         class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
                                         <?php echo $row['Type'];?></label>
-                                    <select id="small-input"
+                                    <select id="small-input" name="type"
                                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                                         <option selected>Choose a data type</option>
                                         <option value="name">Name</option>
@@ -199,6 +199,7 @@ $selectedDatabase = isset($_GET['databases']) ? $_GET['databases'] : null;
                                         <option value="hexColor">Hex Color</option>
                                         <option value="rgbColor">RGB Color</option>
                                         <option value="sentences">Sentences</option>
+                                        <option value="uuid">UUID</option>
                                     </select>
                                 </div>
                             </div>
--- insert.php
+++ insert.php
@@ -1,19 +1,43 @@
 <?php 
 
+
 require_once 'vendor/autoload.php';
+require_once 'seeder.php';
+
 session_start();
-if (isset($_POST['submit'])){
-    $database = $_POST['selectedDatabase'];
-    $table = $_POST['selectedTable'];
-    $user = 'root';
-    $pass = '';
-    $host = 'localhost';
-    $db = new mysqli($host, $user, $pass, $database);
 
-    
+if (isset($_POST['submit'])) {
+    $selectedDatabase = $_POST['selectedDatabase'];
+    $selectedTable = $_POST['selectedTable'];
+    $numberOfEntries = $_POST['numberOfEntries'];
+
+    $host = $_SESSION['host'];
+    $user = $_SESSION['user'];
+    $pass = $_SESSION['pass'];
+
+    $db = new mysqli($host, $user, $pass, $selectedDatabase);
+
+    if ($db->connect_error) {
+        die('Connection failed: ' . $db->connect_error);
+    }
+
+    $seeder = new DatabaseSeeder($db);
+
+    $fields = [];
+
+    // Get the list of fields for the selected table
+    $result = $db->query("DESCRIBE $selectedTable");
+
+    while ($row = $result->fetch_assoc()) {
+        $fields[$row['Field']] = $row['Type'];
+    }
+
+    // Populate the table with the specified number of entries
+    $seeder->seed($selectedTable, $fields, $numberOfEntries);
+
     $db->close();
+
+
 }
-
-
 
 ?>
