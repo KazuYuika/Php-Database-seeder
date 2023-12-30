59140308df02aa13c6a50fd27178dbad25d51e0b
yuki <marinaolouis18@gmail.com>
2023-12-30 16:06:54 +0800

    aaded hints

--- sqlCanvas.php
+++ sqlCanvas.php
@@ -76,7 +76,7 @@ if (isset($_GET['sql_query'])) {
                     <div class="sql-canvas">
                         <form action="sqlCanvas.php" method="get">
                         <input type="text" name="databases" value="<?php echo $_GET['databases'];?>" hidden>
-                        <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your Sql command</label>
+                        <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your Sql command (use CRTL+SPACE for hints)</label>
 <textarea name="sql_query" id="sql-canvas" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Type your SQL query here...">
    
 </textarea>
